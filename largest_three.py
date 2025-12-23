bbe431b96e683373e1725bc19d1168898c40c633
kajalJoshi1 <kajaljoshi764@gmail.com>
2025-12-23 19:29:06 +0430

    Create largest_three.py

---
+++ largest_three.py
@@ -0,0 +1,10 @@
+a=int(input("enter first no:"))
+b=int(input("enter second no:"))
+c=int(input("enter third no:"))
+if(a>b and a>c):
+ print("a is greater")
+elif(b>a and b>c):
+ print("b is greater")
+else:
+ print("c is greater")   
+    
